8517231278a6b76d4458b106e025aa8cf20839f8
seejoshcode@gmail.com <seejoshcode@gmail.com>
2018-01-15 20:35:18 -0500

    added gulp-sass and configured gulpfile.js to use it

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require("gulp"),
+  sass = require("gulp-sass"),
   browserSync = require("browser-sync").create();
 
 gulp.task("serve", function() {
@@ -9,10 +10,17 @@ gulp.task("serve", function() {
     notify: false
   });
 
-  gulp.watch("./css/*.css").on("change", browserSync.reload)
-  gulp.watch("./*.html").on("change", browserSync.reload)
-  gulp.watch("./js/functions.js").on("change", browserSync.reload)
-
+  gulp.watch("./sass/*.scss", ["sass"]);
+  gulp.watch("./*.html").on("change", browserSync.reload);
+  gulp.watch("./js/functions.js").on("change", browserSync.reload);
 });
 
-gulp.task('default', ['serve'])
+gulp.task("sass", function() {
+  return gulp
+    .src("./sass/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("default", ["sass", "serve"]);
