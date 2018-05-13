d71c5f1616f41dea5613687f37b1673ab44e8181
seejoshcode@gmail.com <seejoshcode@gmail.com>
2018-05-13 15:24:43 -0400

    made some changes to copy. Changed some styling to the projets section

---
+++ php/mailer.php
@@ -0,0 +1,41 @@
+<?php
+require 'PHPMailerAutoload.php';
+require '../../phpconfig/config.php'
+
+$trap = $_POST['ruse'];
+
+if($trap) {
+     die();
+} else {
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$msg = $_POST['message'];
+$address = $username;
+
+$m = new PHPMailer;
+
+$m->isSMTP();
+$m->isHTML(true);
+$m->SMTPSecure = 'ssl';
+$m->SMTPAuth = true;
+//$m->SMTPDebug = 2;
+
+$m->Host = 'tls://smtp.gmail.com:587';
+$m->Username = $Username;
+$m->Password = $password;
+$m->addAddress($address, $username);
+
+$m->setFrom($email , $name);
+$m->addReplyTo($email, $name);
+$m->Subject = 'New message from ' . $name;
+$m->WordWrap = 50;
+$m->Body = $msg;
+
+    if ($m->send()) {
+        echo json_encode(42);
+    } else {
+        echo 'Mailer Error: ' . $m->ErrorInfo;
+    }
+}
+?>
